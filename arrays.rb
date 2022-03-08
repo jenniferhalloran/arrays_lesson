d8c3db1ebf61b385cbb3152834929ac1668b7730
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-08 14:21:00 -0500

    Add various arrays

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+# Array of Strings
+months = ["May", "June", "July", "August" "September"]
+
+# Array of Integers
+days = [1, 2, 3, 4, 5, 6, 7, 8]
+
+#Array of Floats
+gpas = [3.7, 3.0, 4.0, 3.2, 2.9]
+
+#Array of Booleans
+booleans = [true, false, false, true, false, true]
