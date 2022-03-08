652a6f27e62b24f7024532a2364b8c7dd54652b4
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-08 14:29:47 -0500

    Add array methods

--- arrays.rb
+++ arrays.rb
@@ -1,11 +1,24 @@
 # Array of Strings
 months = ["May", "June", "July", "August" "September"]
+# Calling pop on this array will remove the last element off the array and return its value/content.
+# In this scenario, it would remove and return September
+months.pop
 
 # Array of Integers
 days = [1, 2, 3, 4, 5, 6, 7, 8]
+# Calling push on the array above with push an elemment to the end of the array and return the updated array.
+#In this scenario, it would push the argument 9 to the end of the days array.
+days.push(9)
+
 
 #Array of Floats
 gpas = [3.7, 3.0, 4.0, 3.2, 2.9]
+#Calling shift on this array will remove the first element of the array and return it.
+# In this scenario, it would return 3.7
+gpas.shift
 
 #Array of Booleans
 booleans = [true, false, false, true, false, true]
+# Calling unshift on this array will add an element to the beginning of the array.
+# In this scenario, it would append true to the front into index position 0.
+booleans.unshift(true)
